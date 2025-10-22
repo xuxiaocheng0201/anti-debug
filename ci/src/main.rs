8ede291711e44d72619cff2b09e04dbb3d9d3330
XuXiaoCheng <2762267080@qq.com>
2025-10-22 22:42:47 +0800

    fix(ci): 修复api错误

--- ci/src/main.rs
+++ ci/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let enable = std::env::var("ANTI_DEBUG").is_ok();
-    if enable && anti_debug::is_debugger_present() {
+    if enable && anti_debug::is_debugger_present().unwrap_or(false) {
         panic!("debugger detected");
     }
 }
