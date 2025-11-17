8a9aeee9836943aa24220fceb80bebcb60e61e29
XuXiaoCheng <2762267080@qq.com>
2025-11-17 23:35:43 +0800

    feat(ci): 添加测试支持

---
+++ examples/ci_deny_attach.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let enable = std::env::var("ANTI_DEBUG").is_ok();
+    if enable {
+        anti_debug::deny_attach().unwrap();
+    }
+    if let Ok(debugger) = std::env::var("DEBUGGER") {
+        let id = std::process::id();
+        let code = match debugger.as_str() {
+            "lldb" => std::process::Command::new("lldb")
+                .args([
+                    "-p", id.to_string().as_str(),
+                    "--batch",
+                    "-o", "\"detach\"",
+                    "-o", "\"quit\"",
+                ])
+                .spawn().unwrap()
+                .wait().unwrap(),
+            "gdb" => std::process::Command::new("gbd")
+                .args([
+                    "-p", id.to_string().as_str(),
+                    "--batch",
+                    "-ex", "\"detach\"",
+                    "-ex", "\"quit\"",
+                ])
+                .spawn().unwrap()
+                .wait().unwrap(),
+            _ => panic!("Unknown debugger"),
+        };
+        assert!(code.success(), "{code}");
+    }
+}
